071e252aec1822e0023d19158043f265a4f9ee7f
Piotr Pacyna <piotr@pacyna.dev>
2020-05-06 17:40:47 +0200

    Create Template Method example

---
+++ DesignPatterns/Behavioral/TemplateMethod/BaseClass.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.Behavioral.TemplateMethod
+{
+    public abstract class BaseClass
+    {
+        // Template method
+        public void TemplateMethod()
+        {
+            Method1();
+            Method2();
+            Method3();
+        }
+
+        protected abstract void Method1();
+        protected abstract void Method2();
+
+        protected virtual void Method3()
+        {
+            //Do something in base class
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/TemplateMethod/Main.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.Behavioral.TemplateMethod
+{
+    public class Main
+    {
+        public void Call()
+        {
+            var example = new ExampleClass();
+            example.TemplateMethod();
+        }
+    }
+}
