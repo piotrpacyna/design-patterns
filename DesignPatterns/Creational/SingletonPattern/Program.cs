aa09c1673ec87370fc881b508e46ca636f30f4ee
Piotr Pacyna <piotr@pacyna.dev>
2020-05-07 23:42:44 +0200

    Create Singleton pattern

---
+++ DesignPatterns/Creational/SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instanceOne = Singleton.Instance;
+            var instanceTwo = Singleton.Instance;
+
+            if (instanceOne == instanceTwo)
+            {
+                Console.WriteLine("Instances are the same");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatterns/Creational/SingletonPattern/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using DesignPatterns.Core;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object syncLock = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance != null)
+                {
+                    LogHelper.Log("Instance already created");
+                    return instance;
+                }
+
+                lock (syncLock)
+                {
+                    instance ??= new Singleton();
+                }
+
+                LogHelper.Log("Instance created");
+                return instance;
+            }
+        }
+    }
+}
